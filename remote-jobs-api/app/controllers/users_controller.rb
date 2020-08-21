838dfdd708c5ff30a3f56f9617f31663ab83502b
github username <um_2003_8@hotmail.com>
2020-08-21 19:17:08 -0400

    added index action

--- remote-jobs-api/app/controllers/users_controller.rb
+++ remote-jobs-api/app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+    def index
+        users = User.all
+        render json: users
+    end
+
     def create
         user = User.new(user_params)
         render json: user.save? user : {errors: 'Something went wrong. Please try again.'}
