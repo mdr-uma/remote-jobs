90cc2324cb0abcf06192fbbd27ee852c14473fee
github username <um_2003_8@hotmail.com>
2020-08-21 19:19:10 -0400

    setup seedfile

--- remote-jobs-api/db/seeds.rb
+++ remote-jobs-api/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: "uma@gmail.com", password: "password")
+User.create(email: "collin@gmail.com", password: "password")
+
+Job.create(url: "url", title: "title", company_name: "lkajsf", job_type: "lakjsf", candidate_required_location: "lkajsf", salary: "lkajsdf", description: ";lakjsf")
+Job.create(url: "url2", title: "title", company_name: "lkajsf", job_type: "lakjsf", candidate_required_location: "lkajsf", salary: "lkajsdf", description: ";lakjsf")
