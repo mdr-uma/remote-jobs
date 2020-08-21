faaf117440f23d04de2b82d19acbf3961e34da07
github username <um_2003_8@hotmail.com>
2020-08-21 19:17:59 -0400

    update realtionship

--- remote-jobs-api/app/models/user.rb
+++ remote-jobs-api/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-    has_and_belongs_to_many :jobs
+    has_many :users_jobs
+    has_many :jobs, through: :users_jobs
 
     validates :email, presence: true, uniqueness: true 
     validates :password, presence: true 
