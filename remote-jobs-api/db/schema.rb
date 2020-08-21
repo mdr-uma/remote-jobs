0d11e79af8cd81772dae8363dc46a1cd0ce04e34
github username <um_2003_8@hotmail.com>
2020-08-21 19:18:48 -0400

    update schema with join table

--- remote-jobs-api/db/schema.rb
+++ remote-jobs-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_21_192949) do
+ActiveRecord::Schema.define(version: 2020_08_21_222732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,18 +27,20 @@ ActiveRecord::Schema.define(version: 2020_08_21_192949) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "jobs_users", id: false, force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "job_id", null: false
-    t.index ["job_id", "user_id"], name: "index_jobs_users_on_job_id_and_user_id"
-    t.index ["user_id", "job_id"], name: "index_jobs_users_on_user_id_and_job_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users_jobs", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "job_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["job_id"], name: "index_users_jobs_on_job_id"
+    t.index ["user_id"], name: "index_users_jobs_on_user_id"
   end
 
 end
