94111b65e7da43ce2d86e21d3c076c1fc10730d5
github username <um_2003_8@hotmail.com>
2020-08-21 13:25:00 -0400

    define create action

--- remote-jobs-api/app/controllers/users_controller.rb
+++ remote-jobs-api/app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+    def create
+        user = User.new(user_params)
+    end
+
+    private
+
+    def user_params
+        params.require(:quote).permit(:email, :password)
+    end
+
 end
