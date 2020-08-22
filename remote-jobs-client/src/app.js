7d8bd8c1adeeaf684e940d4d435974a874275e71
github username <um_2003_8@hotmail.com>
2020-08-22 16:27:53 -0400

    import login form and render it

--- remote-jobs-client/src/app.js
+++ remote-jobs-client/src/app.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react'
+import LoginForm from './components/LoginForm'
 
 class App extends Component{
     render(){
         return(
             <div>
-                Hello
+                <LoginForm/>
             </div>
         )
     }
