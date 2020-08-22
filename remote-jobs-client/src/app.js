138108d6677ab8ac67ef92a6aac924116082b3ed
github username <um_2003_8@hotmail.com>
2020-08-22 19:39:54 -0400

    import sign up form

--- remote-jobs-client/src/app.js
+++ remote-jobs-client/src/app.js
@@ -1,11 +1,30 @@
 import React, {Component} from 'react'
-import LoginForm from './components/LoginForm'
+import SignUpForm from './components/SignUpForm'
 
 class App extends Component{
     render(){
         return(
-            <div>
-                <LoginForm/>
+            <div className="grid-container">
+                <header>
+                    <h2>Remote Jobs</h2>
+                </header>
+                <main>
+                    <div className="content">
+                        <div className="job-list">
+                            <i class='fas fa-clock'></i>
+                            <i className='fas fa-laptop-house'></i>
+                            <i className='fas fa-coffee'></i>
+                            <hr></hr>
+                            <h1>Find the best Remote Job</h1>
+                        </div>
+                        <div className="sidebar">
+                            <SignUpForm/>
+                        </div>
+                    </div>
+                </main>
+                <footer>
+                    Copyright © Remote Jobs 2020
+                </footer>
             </div>
         )
     }
