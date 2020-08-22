8ccf0e9156f6223db6ff649616e93c331da31b1e
github username <um_2003_8@hotmail.com>
2020-08-21 23:07:40 -0400

    setup store, provider, thunk, router and reducers

--- remote-jobs-client/src/index.js
+++ remote-jobs-client/src/index.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app'
+import {createStore, applyMiddleware, compose} from 'redux'
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk'
+import {BrowserRouter as Router} from 'react-router-dom'
+import manageUsersAndJobs from './reducers/manageUsersAndJobs'
 import './index.css'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(manageUsersAndJobs, composeEnhancers(applyMiddleware(thunk)))
+
 ReactDOM.render(
-<App/>,
+<Provider store={store}>
+    <Router>
+        <App/>
+    </Router>
+</Provider>,
 document.getElementById('root')
 )
