68bf76315102c383bc7944ca269a4e7a67cc4e36
github username <um_2003_8@hotmail.com>
2020-08-22 16:30:38 -0400

    create usersReducer function

---
+++ remote-jobs-client/src/reducers/usersReducer.js
@@ -0,0 +1,22 @@
+export default function usersReducer(state = {
+    users:[],
+    loading:false
+}, action){
+
+    switch (action.type) {
+        case 'LOADING_USERS':
+            return {
+                ...state,
+                users: [...state.users],
+                loading: true
+            }
+        case 'CREATE_USERS':
+            return {
+                ...state,
+                users: action.users,
+                loading: false
+            }
+        default:
+            return state
+    }
+}
