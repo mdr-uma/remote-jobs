361aadc54b8a4e1175bd20f13f69aea12deec675
github username <um_2003_8@hotmail.com>
2020-08-22 21:54:30 -0400

    create bodyImage component

---
+++ remote-jobs-client/src/components/BodyImage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const BodyImage = () => {
+    return(
+        <div className="job-list">
+            <i class='fas fa-clock'></i>
+            <i className='fas fa-laptop-house'></i>
+            <i className='fas fa-coffee'></i>
+            <hr></hr>
+            <h1>Find the best Remote Job</h1>
+        </div>
+    )
+}
+
+export default BodyImage
