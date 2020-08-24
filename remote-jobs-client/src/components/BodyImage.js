eeca35f36b353a721e93d943916c818c963dfcb8
github username <um_2003_8@hotmail.com>
2020-08-24 00:05:38 -0400

    added image

--- remote-jobs-client/src/components/BodyImage.js
+++ remote-jobs-client/src/components/BodyImage.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import image from '../images/img-2.jpg'
 
 const BodyImage = () => {
     return(
         <div className="job-list">
-            <i class='fas fa-clock'></i>
-            <i className='fas fa-laptop-house'></i>
-            <i className='fas fa-coffee'></i>
+            {/* <i className='fas fa-laptop-house'></i> */}
+            <img src={image} alt=""/>
             <hr></hr>
             <h1>Find the best Remote Job</h1>
         </div>
