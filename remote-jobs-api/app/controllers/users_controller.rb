33d5076cd6b413bb7b83937d44f42595ba0f89f0
github username <um_2003_8@hotmail.com>
2020-08-25 12:02:31 -0400

    updated for user authentication

--- remote-jobs-api/app/controllers/users_controller.rb
+++ remote-jobs-api/app/controllers/users_controller.rb
@@ -1,14 +1,39 @@
 class UsersController < ApplicationController
+    before_action :authenticate_user, only: [:show]
+
     def index
-        users = User.all
-        render json: users
+        @users = User.all
+        if @users
+            render json: @users
+        else 
+            render json: {
+                error: 'no users found'
+            }, status: 500
+        end
     end
 
+    # def show
+    #     @user = User.find_by(id: params[:id])
+    #     if @user
+    #         render json: @user 
+    #     else
+    #         render json: {
+    #             status: 500,
+    #             errors: ['no users found']
+    #         }
+    #     end
+    # end
+
     def create
-        user = User.new(user_params)
-        render json: user.save ? user : {error: 'something went wrong'}
-
-
+        @user = User.create(user_params)
+        if @user.valid?
+            created_jwt = issue_token({id: @user.id})
+            cookies.signed[:jwt] = {value: created_jwt, httponly: true, expires: 1.hour.from_now}
+            render json: @user
+        else
+            rendre json: {error: "Somthing went wrong, Please try again later."}
+        end
+        # render json: user.save ? user : {error: 'something went wrong'}
     end
 
     private
--- remote-jobs-api/app/models/user.rb
+++ remote-jobs-api/app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
     has_many :users_jobs
     has_many :jobs, through: :users_jobs
 
+    has_secure_password
+
     validates :email, presence: true, uniqueness: true 
     validates :password, presence: true 
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
