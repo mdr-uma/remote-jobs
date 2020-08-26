4a1baffa36d2356161ae654ec0d0b77ace1c83eb
github username <um_2003_8@hotmail.com>
2020-08-25 20:02:28 -0400

    create login form

---
+++ remote-jobs-client/src/components/LoginForm.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { loginUser } from '../actions/loginUser'
+import { connect } from 'react-redux'
+// import image from '../images/image-1.jpeg'
+
+class LoginForm extends React.Component {
+    state = {
+        email: "",
+        password: ""
+    }
+
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault()
+        this.props.loginUser(this.state)
+        this.setState({
+            email: "",
+            password: ""
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="sidebar">
+                    <div className="form-container">
+                        <h3>Login</h3>
+                        <form onSubmit={this.handleSubmit}>
+                            <label>Email Address: </label>
+                            <input type="text" placeholder="eg@gmail.com" value={this.state.email} name="email" onChange={this.handleChange} /> <br /><br />
+                            <label>Password: </label>
+                            <input type="password" placeholder="password" value={this.state.password} name="password" onChange={this.handleChange} /> <br /><br />
+                            <input type="submit" value="login"/>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+
+export default connect(null, { loginUser })(LoginForm)
