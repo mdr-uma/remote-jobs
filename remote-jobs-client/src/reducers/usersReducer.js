0bed63275f4bbc21449427ed03a78053e7c0a7d2
github username <um_2003_8@hotmail.com>
2020-08-27 12:38:58 -0400

    added LOGIN_USER case

--- remote-jobs-client/src/reducers/usersReducer.js
+++ remote-jobs-client/src/reducers/usersReducer.js
@@ -1,7 +1,11 @@
 export default function usersReducer(state = {}, action){
-
     switch (action.type) {
         case 'CREATE_USERS':
+            return {
+                user: action.payload
+            }
+
+        case 'LOGIN_USER':
             return {
                 user: action.payload
             }
