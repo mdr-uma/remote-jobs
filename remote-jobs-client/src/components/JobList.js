344dbb421608b2e156f56327fc4906b2f8c56031
github username <um_2003_8@hotmail.com>
2020-08-28 18:12:44 -0400

    added few more attributes to job model

--- remote-jobs-client/src/components/JobList.js
+++ remote-jobs-client/src/components/JobList.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import Button from './Button'
 
 const renderJobs = (jobs) => {
     return jobs.map((job, index) => {
         return (
-            <div key={index}>
-              <h1>{job.company}</h1>
-                <a alt="job link" href={job.url}>Job links</a>
-              <h4>{job.description}</h4>
+            <div key={index} className="all-jobs">
+                {/* <img src={job.logo} alt="" className="logo"/> */}
+                <h5>{job.date}</h5>
+                <h3>{job.company}</h3>
+                <h4>{job.position}</h4>
+                <a alt="job link" href={job.url}>{job.url}</a>
+                <h6>{job.description}</h6>
+                <Button className="button"/>
             </div>
         )
     })
@@ -15,8 +20,7 @@ const renderJobs = (jobs) => {
 const JobList = props => {
     // console.log("props",props);
     return(
-        <div>
-            <p>Joblist</p>
+        <div className="job-container">
             {renderJobs(props.jobs)}
         </div>
     )
