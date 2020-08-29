3ca72e01201dc5abe783b2ce5eefd266e08eb21d
github username <um_2003_8@hotmail.com>
2020-08-29 01:58:58 -0400

    fetch search api

--- remote-jobs-client/src/actions/fetchJobs.js
+++ remote-jobs-client/src/actions/fetchJobs.js
@@ -1,5 +1,12 @@
-export const fetchJobs = () => async (dispatch) => {
+export const fetchJobs = () => async(dispatch) => {
     const res = await fetch('https://remoteok.io/api?ref=producthunt')
     .then(res => res.json())
     dispatch({type: 'ADD_JOBS', jobs: res}) 
+}
+
+export const fetchSearchJobs = (jobs, callback) => async(dispatch) => {
+    const response = await fetch(`https://remoteok.io/api?tags=${jobs}`)
+    .then(res => res.json())
+    dispatch({type: 'SEARCH_JOBS', jobs: response})
+    callback()
 }
