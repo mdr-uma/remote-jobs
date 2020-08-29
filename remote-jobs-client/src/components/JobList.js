9857a1c172253867bc9c3ea7ac1eee2eff577713
github username <um_2003_8@hotmail.com>
2020-08-28 20:53:13 -0400

    change date format

--- remote-jobs-client/src/components/JobList.js
+++ remote-jobs-client/src/components/JobList.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Button from './Button'
+import dateFormat from 'dateformat'
+
+// const dateOption = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' }
 
 const renderJobs = (jobs) => {
     return jobs.map((job, index) => {
+        let date = job.date
         return (
             <div key={index} className="all-jobs">
                 {/* <img src={job.logo} alt="" className="logo"/> */}
-                <h5>{job.date}</h5>
+                <h5>{dateFormat(date,"mmmm dS, yyyy")}</h5>
                 <h3>{job.company}</h3>
                 <h4>{job.position}</h4>
                 <a alt="job link" href={job.url}>{job.url}</a>
