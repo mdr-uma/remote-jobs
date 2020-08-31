a2b59f55bbda8ea9eaa8f2348c24cb4f40b588e1
github username <um_2003_8@hotmail.com>
2020-08-31 15:40:12 -0400

    add images

--- remote-jobs-client/src/components/About.js
+++ remote-jobs-client/src/components/About.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Container, Row, Col } from 'react-bootstrap'
+import image from '../images/img-4.jpg'
+import img5 from '../images/img-5.jpg'
 
 const About = () => {
     return(
         <div>
             <Container>
                 <Row>
-                    <Col xs={1}>
-                        <p>images images mages imagesmages imagesmages imagesmages imagesmages imagesmages imagesmages imagesmages imagesmages imagesmages imagesmages imagesmages imagesmages imagesmages imagesmages imagesmages imagesmages images</p>
+                    <Col md= "2">
+                        <img src={image} alt="" />
+                        <img src={img5} alt="" />
                     </Col>
-                    <Col lg={10}>
-                        <p>about usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout us</p>
+                    <Col lg={8}>
+                        about usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout 
+                            usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout
+                             usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout us
                     </Col>
                 </Row>
             </Container>
