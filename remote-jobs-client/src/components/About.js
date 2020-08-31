9dcff40ce84064ab70b7b095a523f197a2584b57
github username <um_2003_8@hotmail.com>
2020-08-31 01:26:12 -0400

    import bootstrap react and create column

--- remote-jobs-client/src/components/About.js
+++ remote-jobs-client/src/components/About.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container, Row, Col } from 'react-bootstrap'
 
 const About = () => {
     return(
         <div>
-            <h1>About Page</h1>
+            <Container>
+                <Row>
+                    <Col xs={1}>
+                        <p>images images mages imagesmages imagesmages imagesmages imagesmages imagesmages imagesmages imagesmages imagesmages imagesmages imagesmages imagesmages imagesmages imagesmages imagesmages imagesmages imagesmages images</p>
+                    </Col>
+                    <Col lg={10}>
+                        <p>about usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout us</p>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
