b15d05fd80c84af6bf89bc0d1262dbbf9f90eb69
github username <um_2003_8@hotmail.com>
2020-08-31 01:25:02 -0400

    render jwt token with json

--- remote-jobs-api/app/controllers/sessions_controller.rb
+++ remote-jobs-api/app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
         if @user && @user.authenticate(params[:user][:password])
             created_jwt = issue_token({id: @user.id})
             cookies.signed[:jwt] = {value: created_jwt, httponly: true, expires: 1.hour.from_now}
-            render json: @user
+            render json: {username: @user.username, id: @user.id, token: cookies.signed[:jwt]}
         else
             render json: {
                 error: 'Email or Password Incorrect.'
