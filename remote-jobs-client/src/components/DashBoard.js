5a885ed3717d1998b4bf95abb42534f2ee3097f9
github username <um_2003_8@hotmail.com>
2020-08-31 01:28:07 -0400

    remove whitespace

--- remote-jobs-client/src/components/DashBoard.js
+++ remote-jobs-client/src/components/DashBoard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const DashBoard = (props) => {
     return(
-        <div className= "content">
+        <div className="content">
             <div className="job-list">
                 <h1> Welcome to your DashBoard.</h1>
                 <p>You can see all your saved jobs here. Good Luck on Job hunt!</p>
@@ -14,6 +14,7 @@ const DashBoard = (props) => {
 
             </div>
         </div>
+
         
     )
 }
