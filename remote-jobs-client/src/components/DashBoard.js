c6f4ab85d1a3b1d432e4ad9413c8716e673dd1cb
github username <um_2003_8@hotmail.com>
2020-08-30 22:07:52 -0400

    added some divs and classNames

--- remote-jobs-client/src/components/DashBoard.js
+++ remote-jobs-client/src/components/DashBoard.js
@@ -2,9 +2,17 @@ import React from 'react'
 
 const DashBoard = (props) => {
     return(
-        <div>
-            <h1>Hi {props.userName}, Welcome to your DashBoard.</h1>
-             <p>You can see all your saved job post here. Good Luck!</p>
+        <div className= "content">
+            <div className="job-list">
+                <h1> Welcome to your DashBoard.</h1>
+                <p>You can see all your saved jobs here. Good Luck on Job hunt!</p>
+            </div>
+            <div className="side-bar">
+                Hi, {props.userName}!
+            </div>
+            <div className="saved-job">
+
+            </div>
         </div>
         
     )
