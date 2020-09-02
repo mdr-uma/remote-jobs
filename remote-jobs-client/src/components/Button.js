590727f5e0eea61696f2831fa59f2a546ad113db
github username <um_2003_8@hotmail.com>
2020-09-01 20:11:32 -0400

    connect to redux store

--- remote-jobs-client/src/components/Button.js
+++ remote-jobs-client/src/components/Button.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import { useHistory } from "react-router-dom";
+import {connect} from "react-redux"
+import {savedJobs} from '../actions/fetchJobs'
 
 
 const Button = (props) => {
     const history = useHistory()
-    
     const routeChange =()=> {
         history.push(`/dashboard`)
     }
     return(
         <div>
-            <i className="fas fa-save" onClick={routeChange}></i>
-            {/* {props.buttonText} */}
+            <i className="fas fa-save" onClick={() => props.savedJobs(props.job, props.user, routeChange)}></i>
         </div>
     )
 }
 
-export default Button
+const mapStateToProps = ({user}) => {
+    return {
+        user
+    }
+}
+
+export default connect(mapStateToProps, {savedJobs})(Button)
