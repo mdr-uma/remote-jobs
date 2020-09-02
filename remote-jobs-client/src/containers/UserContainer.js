6d091c22cc2e01d0eca5804b2ef27fbcde7d01ff
github username <um_2003_8@hotmail.com>
2020-09-01 20:15:36 -0400

    connect logout user action creator and pass it as a props

--- remote-jobs-client/src/containers/UserContainer.js
+++ remote-jobs-client/src/containers/UserContainer.js
@@ -1,21 +1,27 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import DashBoard from '../components/DashBoard'
+import {logoutUser} from '../actions/logoutUser'
 
 class UserContainer extends React.Component{
     render() {
         return(
             <div>
-                <DashBoard userName={this.props.user.username}/>
+                <DashBoard 
+                userName={this.props.user.username} 
+                logOut={this.props.logoutUser} 
+                savedJobs={this.props.savedJobs}
+                />
             </div>
         )
     }
 }
 
-const mapStateToProps = ({ user }) => {
+const mapStateToProps = ({ user, savedJobs }) => {
     return {
-        user
+        user,
+        savedJobs
     }
 }
 
-export default connect(mapStateToProps)(UserContainer)
+export default connect(mapStateToProps, {logoutUser})(UserContainer)
