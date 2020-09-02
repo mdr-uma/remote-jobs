f4088c03c8676ad0d6fa98bcb081fe1524315905
github username <um_2003_8@hotmail.com>
2020-09-01 20:09:42 -0400

    create savedJobs function

--- remote-jobs-client/src/actions/fetchJobs.js
+++ remote-jobs-client/src/actions/fetchJobs.js
@@ -10,3 +10,32 @@ export const fetchSearchJobs = (jobs, callback) => async(dispatch) => {
     dispatch({type: 'SEARCH_JOBS', jobs: response})
     callback()
 }
+
+export const savedJobs = (job, user, callback) => {
+
+    return (dispatch) => {
+    return fetch(`http://localhost:3000/jobs`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ 
+            job: {
+                 url: job.url, 
+                 company:job.company, 
+                 date:job.date, 
+                 position:job.position, 
+                 description:job.description, 
+                 user_id: user.id
+            }
+        })
+    })
+    .then(res=>res.json())
+    .then(data => {
+        dispatch({ type: "SAVED_JOB", job: data })
+        callback()
+        })
+    }
+}
