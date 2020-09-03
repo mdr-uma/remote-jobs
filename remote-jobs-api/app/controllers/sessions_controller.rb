23f5e24eb0f531c1885aa86a9ba2ff6cd1b06cbf
github username <um_2003_8@hotmail.com>
2020-09-03 12:31:04 -0400

    difine currentUser method

--- remote-jobs-api/app/controllers/sessions_controller.rb
+++ remote-jobs-api/app/controllers/sessions_controller.rb
@@ -4,7 +4,8 @@ class SessionsController < ApplicationController
         if @user && @user.authenticate(params[:user][:password])
             created_jwt = issue_token({id: @user.id})
             cookies.signed[:jwt] = {value: created_jwt, httponly: true, expires: 1.hour.from_now}
-            render json: {username: @user.username, id: @user.id, token: cookies.signed[:jwt]}
+            # render json: {username: @user.username, id: @user.id, token: cookies.signed[:jwt]}
+            render json: @user
         else
             render json: {
                 error: 'Email or Password Incorrect.'
@@ -13,6 +14,15 @@ class SessionsController < ApplicationController
     end
 
     def destroy
+        # binding.pry
         cookies.delete(:jwt)
+    end
+
+    def currentUser
+        if logged_in?
+            render json: serializer_user(current_user)
+        else
+            render json: {error: "No current user"}
+        end
     end
 end
