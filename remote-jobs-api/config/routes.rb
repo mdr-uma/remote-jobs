6f15fb6d23d15ba6a8944506141f8b7e1e419056
github username <um_2003_8@hotmail.com>
2020-09-03 12:29:22 -0400

    add currentUser route

--- remote-jobs-api/config/routes.rb
+++ remote-jobs-api/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
     post '/login' => "sessions#login"
     delete '/logout', to: 'sessions#destroy'
+    get '/currentUser' => "sessions#currentUser"
 
 resources :users, only: [:create, :index, :show]
 resources :jobs, except: :update
