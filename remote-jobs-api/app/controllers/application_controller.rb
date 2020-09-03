cce5d3db38a8e19e4aa4012636662098be2c8ade
github username <um_2003_8@hotmail.com>
2020-09-03 12:30:37 -0400

    define current_user, logged_in? and seralizer_user method

--- remote-jobs-api/app/controllers/application_controller.rb
+++ remote-jobs-api/app/controllers/application_controller.rb
@@ -1,13 +1,25 @@
 class ApplicationController < ActionController::API
     include ::ActionController::Cookies
 
+    def current_user
+        @current_user ||= User.find_by(id: authenticate_user)
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def serializer_user(user)
+        { username: user.username, email:user.email, id: user.id, savedJobs: user.jobs}
+    end
+
     def issue_token(payload)
         JWT.encode(payload, "secret", "HS256")
     end
 
     def authenticate_user
         jwt = cookies.signed[:jwt]
-        JWT.decode(jwt, "secret", "HS256")
+        JWT.decode(jwt, "secret", "HS256")[0]["id"]
     end
 end
 
