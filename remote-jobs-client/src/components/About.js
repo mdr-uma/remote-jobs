0c4985ded20192f652d29eb79a39182e2c944150
github username <um_2003_8@hotmail.com>
2020-09-05 13:52:13 -0400

    add paragraphs

--- remote-jobs-client/src/components/About.js
+++ remote-jobs-client/src/components/About.js
@@ -1,25 +1,26 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { Container, Row, Col } from 'react-bootstrap'
 import image from '../images/img-4.jpg'
 import img5 from '../images/img-5.jpg'
 
 const About = () => {
     return(
-        <div>
-            <Container>
-                <Row>
-                    <Col md= "2">
-                        <img src={image} alt="" />
-                        <img src={img5} alt="" />
-                    </Col>
-                    <Col lg={8}>
-                        about usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout 
-                            usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout
-                             usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout us
-                    </Col>
-                </Row>
-            </Container>
+        <div className="about-page">
+
+            <h3>About Us</h3>
+            <hr /><br/>
+            <img src={img5} alt="" className="left-img"/>
+            <p>This app is for the software engineers who are seeking remote jobs. Due to COVID-19, there are thousands of companies running remotely
+            and I took this as an opportunity to build this application as my final project, which can be beneficial for a lot of software engineers.
+            This app is listing all the remote jobs, sorted by recent date and it's updating everyday. There is also search feature where you can
+            search job by tags. Please do not add space between words, instead use plus(+) sign in search input.
+             </p>
+
+            <img src={image} alt="" className="right-img"/>
+            <p> Users can create accounts to save the jobs they like. One job can be saved only one time. Users who don't have account
+            they can still view all the jobs. Actual job links are provided in the list so you can apply it directly from there.
+            Users who have an account will have a private dashboard where all the saved jobs stay and it's private.
+            </p>
+                   
         </div>
     )
 }
