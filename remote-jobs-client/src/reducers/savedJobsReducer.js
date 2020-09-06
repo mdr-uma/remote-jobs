8e1412586cb06f9e64069a83fd1071c9e8f17e34
github username <um_2003_8@hotmail.com>
2020-09-06 02:08:31 -0400

    added REMOVE_JOB action type

--- remote-jobs-client/src/reducers/savedJobsReducer.js
+++ remote-jobs-client/src/reducers/savedJobsReducer.js
@@ -1,9 +1,14 @@
 const savedJobsReducer = (state = [], action) => {
+    console.log(action.id)
     switch (action.type) {
         case 'SAVED_JOB':
             return (
                 [...state, {job:action.job, save: action.save}]
             )
+        case 'REMOVE_JOB':
+            return (
+                state.filter(job => job.id !== action.id)
+                )
 
         default:
             return state
