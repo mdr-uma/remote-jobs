ff46c1f2b7ebb6b8ccc4bd1e5bea9bae83c04641
github username <um_2003_8@hotmail.com>
2020-09-06 20:08:41 -0400

    changed conditional logic

--- remote-jobs-client/src/components/NavBar.js
+++ remote-jobs-client/src/components/NavBar.js
@@ -7,7 +7,7 @@ const renderNavbar = (user) => {
             <div className="nav-link">
                 <Link className="nav-link" to="/">Home</Link>
                 <Link className="nav-link" to="/about">About</Link>
-                {Object.keys(user).length > 0 ? <Link className="nav-link" to="/dashboard">Dashboard</Link> : <Link className="nav-link" to="/login">Login</Link>}  
+                {user.username ? <Link className="nav-link" to="/dashboard">Dashboard</Link> : <Link className="nav-link" to="/login">Login</Link>}  
             </div>
         )
 } 
