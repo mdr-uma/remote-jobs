5430e4ecbef941ccc416f9535ff9389f9a395bda
github username <um_2003_8@hotmail.com>
2020-09-06 20:05:36 -0400

    added connect to redux store and componentDidMount

--- remote-jobs-client/src/components/App.js
+++ remote-jobs-client/src/components/App.js
@@ -7,14 +7,22 @@ import JobsContainer from '../containers/JobsContainer'
 import Footer from './Footer'
 import UserContainer from '../containers/UserContainer'
 import SearchResult from './SearchResult'
+import {connect} from 'react-redux'
+import { getUser } from '../actions/loginUser'
+
 
 
 class App extends Component{
+
+    componentDidMount() {
+        this.props.getUser()
+    }
+
     render(){
         return(
             <div className="grid-container">
                 <BrowserRouter>
-                <NavBar/> 
+                <NavBar user={this.props.user}/> 
                     <Route exact path='/' component={JobsContainer} /> 
                     <Route exact path='/about' component={About} /> 
                     <Route exact path='/login' component={LoginForm} />
@@ -27,4 +35,11 @@ class App extends Component{
         )
     }
 }
-export default App
+
+const mapStateToProps = ({ user }) => {
+    return {
+        user
+    }
+}
+
+export default connect(mapStateToProps, {getUser})(App)
