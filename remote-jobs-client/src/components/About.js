5d934741d8a2cfeed1b7b3ed87e7eaebecb666c9
github username <um_2003_8@hotmail.com>
2020-09-07 01:05:05 -0400

    About page

--- remote-jobs-client/src/components/About.js
+++ remote-jobs-client/src/components/About.js
@@ -9,16 +9,17 @@ const About = () => {
             <h3>About Us</h3>
             <hr /><br/>
             <img src={img5} alt="" className="left-img"/>
-            <p>This app is for the software engineers who are seeking remote jobs. Due to COVID-19, there are thousands of companies running remotely
-            and I took this as an opportunity to build this application as my final project, which can be beneficial for a lot of software engineers.
-            This app is listing all the remote jobs, sorted by recent date and it's updating everyday. There is also search feature where you can
-            search job by tags. Please do not add space between words, instead use plus(+) sign in search input.
-             </p>
-
+            <p>This app is for the software engineers who are seeking remote jobs. Due to COVID-19, there are thousands 
+            of companies running remotely and I took this as an opportunity to build this application as my final project, 
+            which can be beneficial for a lot of software engineers. This app is listing all the remote jobs, sorted by 
+            recent date and it's updating everyday. There is also a search feature where you can search jobs by tags. 
+            Please do not add space between words, instead use plus(+) sign in search input.  
+            </p>
             <img src={image} alt="" className="right-img"/>
-            <p> Users can create accounts to save the jobs they like. One job can be saved only one time. Users who don't have account
-            they can still view all the jobs. Actual job links are provided in the list so you can apply it directly from there.
-            Users who have an account will have a private dashboard where all the saved jobs stay and it's private.
+            <p>Users can create accounts to save the jobs they are interested on. One job can be saved only one time. 
+            Users who don't have an account can still view all the jobs and apply for it. Actual job links are provided 
+            in the list so you can apply it directly from there. Users who have an account will have a private dashboard 
+            where all the saved jobs stay safely and once you apply for a job it can be remove from dashboard anytime.
             </p>
                    
         </div>
